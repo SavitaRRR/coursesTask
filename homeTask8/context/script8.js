8d9872b35ebbf50eec4bafefc37c3af870c05b1a
Dima chernishov <dimkachernishov@gmail.com>
2017-04-21 02:52:23 +0300

    add framework and context

--- homeTask8/context/script8.js
+++ homeTask8/context/script8.js
@@ -1,2 +1,19 @@
-var contextMenu = document.getElementById('context-menu');
-var menu = document.getElementById('menu');
+var contextMenu = document.getElementById('menuLink').parentNode;
+var doc = document.documentElement;
+
+contextMenu.addEventListener('click', function(e){
+	
+	e.stopPropagation();
+
+});
+doc.addEventListener("contextmenu",function(e){
+	e.preventDefault();
+	contextMenu.style.display ="block";
+	console.log(e.clientY + " " + e.clientX);
+});
+var links = contextMenu.getElementsByTagName("a");
+for(var i = 0;i<links.length;i++){
+	links[i].addEventListener("click",function(){
+		alert(this.innerText);
+	})
+}
